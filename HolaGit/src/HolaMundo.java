41c2bcbb74f6bbde24fa175b5d86ea6da5bc1945
daniel <daniel@manjaro>
2021-03-13 19:16:53 -0500

    ultrasecreto

--- HolaGit/src/HolaMundo.java
+++ HolaGit/src/HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola  mundo con Jenkins, Java y Github con secreto");
+		System.out.println("Hola  mundo con Jenkins, Java y Github con secreto ");
 
 	}
 }
